ce8775bceedbd25dfd41a7f8dd217a2c2a22667f
MMCH <john.doe@example.com>
2020-12-18 11:52:20 +0100

    angry snake

---
+++ titans_snapshot.sh
@@ -0,0 +1,12 @@
+# Blockchain snapshot
+
+SNAPSHOT="https://blockdata-1258895559.cos.ap-shanghai.myqcloud.com/intchainData-20201217.tar.gz"
+
+rm -rf /tmp/intchain*
+echo "Downloading blockchain snapshot from $SNAPSHOT"
+wget -q --show-progress "$SNAPSHOT" -P "/tmp/"
+rm -rf "${HOME}/.intchain/"
+mkdir -p "${HOME}/.intchain/"
+echo "Extracting '/tmp/intchainData-20201217.tar.gz' to '${HOME}/.intchain/'"
+tar -C "${HOME}/.intchain/" -xzvf "/tmp/intchainData-20201217.tar.gz" --strip-components 1 > ~/return.log
+echo "done you can start intchain"
