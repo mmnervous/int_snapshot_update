fbab74b488d1f584f1b05d1ad0ba1139f42220f0
MMCH <john.doe@example.com>
2020-12-18 13:26:56 +0100

    loop fix

--- titans_snapshot.sh
+++ titans_snapshot.sh
@@ -2,10 +2,9 @@
 
 SNAPSHOT="https://blockdata-1258895559.cos.ap-shanghai.myqcloud.com/intchainData-20201217.tar.gz"
 
-while true; do
-	read -p "Did you stop intchain?" yn
-	case $yn in
-		[Yy]* )
+echo -n "Did you stop intchain? (y/n)? "
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
 			rm -rf /tmp/intchain*
 			echo "Downloading blockchain snapshot from $SNAPSHOT"
 			wget -q --show-progress "$SNAPSHOT" -P "/tmp/"
@@ -14,8 +13,6 @@ while true; do
 			echo "Extracting '/tmp/intchainData-20201217.tar.gz' to '${HOME}/.intchain/'"
 			tar -C "${HOME}/.intchain/" -xzvf "/tmp/intchainData-20201217.tar.gz" --strip-components 1 > ~/return.log
 			echo "Almost done is not done. But this is the end of this script. If you see this message it means -"
-			;;
-		[Nn]* ) exit;;
-		* ) echo "Please answer yes or no.";;
-	esac
-done
+else
+    exit
+fi
