4cfba9c11828c0d5d9c283067aab09703c846ea7
MMCH <john.doe@example.com>
2020-12-22 08:38:50 +0100

    update to new file_name

--- titans_snapshot.sh
+++ titans_snapshot.sh
@@ -3,7 +3,7 @@
 SNAPSHOT="http://shaiiko.com/mmch_intchainData-20201222.tar.gz"
 SNAPSHOT_1="http://shaiiko.com/intchainData-20201217.tar.gz"
 SNAPSHOT_2="https://blockdata-1258895559.cos.ap-shanghai.myqcloud.com/intchainData-20201217.tar.gz"
-FILE=/tmp/mmch_intchainData-20201217.tar.gz
+FILE=/tmp/mmch_intchainData-20201222.tar.gz
 
 echo "Last update Fri 22 Dec 2020 8:26 AM CET"
 echo -n "Did you stop intchain? (y/n)? "
