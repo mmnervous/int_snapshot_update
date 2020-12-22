0bf22399b63757dcab931a2f1ec750bbbdb2f775
MMCH <john.doe@example.com>
2020-12-22 08:38:05 +0100

    update to new file_name

--- titans_snapshot.sh
+++ titans_snapshot.sh
@@ -3,7 +3,7 @@
 SNAPSHOT="http://shaiiko.com/mmch_intchainData-20201222.tar.gz"
 SNAPSHOT_1="http://shaiiko.com/intchainData-20201217.tar.gz"
 SNAPSHOT_2="https://blockdata-1258895559.cos.ap-shanghai.myqcloud.com/intchainData-20201217.tar.gz"
-FILE=/tmp/intchainData-20201217.tar.gz
+FILE=/tmp/mmch_intchainData-20201217.tar.gz
 
 echo "Last update Fri 22 Dec 2020 8:26 AM CET"
 echo -n "Did you stop intchain? (y/n)? "
@@ -23,8 +23,8 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
 					echo "$FILE exists."
 					rm -rf "${HOME}/.intchain/"
 					mkdir -p "${HOME}/.intchain/"
-					echo "Extracting '/tmp/mmch_intchainData-20201217.tar.gz' to '${HOME}/.intchain/'"
-					tar -C "${HOME}/.intchain/" -xzvf "/tmp/mmch_intchainData-20201217.tar.gz" --strip-components 1 2>&1 |
+					echo "Extracting '$FILE' to '${HOME}/.intchain/'"
+					tar -C "${HOME}/.intchain/" -xzvf "$FILE" --strip-components 1 2>&1 |
 					while read line; do
 						x=$((x+1))
 						echo -en "$x extracted\r"
