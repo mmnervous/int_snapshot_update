7f26205145e85f3d5d795c5286c438ea7603f4b5
MMCH <john.doe@example.com>
2021-01-09 12:27:42 +0100

    refactoring

--- titans_snapshot.sh
+++ titans_snapshot.sh
@@ -8,9 +8,6 @@ FILE_NAME="mmch_825656_intchainData-20201222.tar.gz"
 SNAPSHOT="http://shaiiko.com/${FILE_NAME}"
 FILE="/tmp/${FILE_NAME}"
 data_dir="${HOME}/.intchain"
-backup_dir="${HOME}/backup"
-backup_file="${backup_dir}/int_backup.tar"
-intchain_pid="$(pidof intchain)"
 
 # Color  Variables
 RED='\e[31m'
@@ -19,30 +16,25 @@ YLW='\e[33m'
 BLU='\e[94m'
 CLR='\e[0m'
 
-echo -e "$BLU Last update Fri 22 Dec 2020 5:46 PM CET | Block height 825,656 $CLR"
-if [[ -z "${intchain_pid}" ]]; then
+if [[ "$(pidof intchain)" ]]; then # checking if intchain is running
+	echo -e "$RED""You have to stop intchain first $CLR"
+	exit
+else
+	echo -e "$BLU""Last update Sat 09 Jan 2021 11:53 AM CET | Block height $CLR"
 
-	# Download
-	echo -en "$YLW Start download (y/n)?  $CLR"
-	read answer
+	read -p "Start download (y/n)? " answer
 	if [ "$answer" != "${answer#[Yy]}" ] ;then
 		rm -rf /tmp/*intchain*
-		echo "Downloading blockchain snapshot from $SNAPSHOT"
 		wget -q --show-progress "$SNAPSHOT" -P "/tmp/"
 	fi
 
-	# Backup
-	echo -en "$YLW Backup your files (y/n)?  $CLR"
-	read answer
+	read -p "Backup your files (y/n)? " answer
 	if [ "$answer" != "${answer#[Yy]}" ] ;then
-		mkdir -p ~/backup
-		rm -rf $backup_file
-		tar -cvf "${backup_file}" $(find ${data_dir} -name "UTC*" -or -name "nodekey" -or -name "priv_validator.json")
+		tmp_dir=$(mktemp -d -t int_backup-XXX)
+		tar -cvf $tmp_dir/backup.tar $(find ${data_dir} -name "UTC*" -or -name "nodekey" -or -name "priv_validator.json")
 	fi
 
-	# Exctraction
-	echo -en "$YLW Start extraction (y/n)?  $CLR"
-	read answer
+	read -p "Start extraction (y/n)? " answer
 	if [ "$answer" != "${answer#[Yy]}" ] ;then
 		if [ -f "$FILE" ]; then
 			echo "$FILE exists."
@@ -50,25 +42,21 @@ if [[ -z "${intchain_pid}" ]]; then
 			mkdir -p "${HOME}/.intchain/"
 			echo "Extracting '$FILE' to '${HOME}/.intchain/'"
 			tar -C "${HOME}/.intchain/" -xzvf "$FILE" --strip-components 1 2>&1 |
-			while read line; do
+			while read line; do # show how much files are extracted
 				x=$((x+1))
 				echo -en "$x extracted\r"
 			done
-			echo -e "$GRN Extraction done ! $CLR"
+			echo -e "$GRN""Extraction done ! $CLR"
 		else
 			echo -e "$FILE $RED does not exist. You have to download it. > EXIT $CLR"
 			exit
 		fi
 	fi
 
-	# restore back up
-	echo -en "$YLW Restore your files (y/n)?  $CLR"
-	read answer
+	read -p "Restore your files (y/n)? " answer
 	if [ "$answer" != "${answer#[Yy]}" ] ;then
-		tar -xvf ${backup_file} -C "/"
+		tar -xvf $tmp_dir/backup.tar -C "/"
 	fi
-	echo -e "$GRN Done ! End of script $CLR"
-else
-	echo -e "$RED You have to stop intchain first > EXIT $CLR"
-	exit
+	echo -e "$GRN""Done ! End of script $CLR"
+
 fi
